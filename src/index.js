cd686f9a97d3dda09719d882b9fc8231fc7a795f
Nicholas Chung <iamnicholaschung@gmail.com>
2020-05-21 14:31:03 +0800

    Enable db and dotenv

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-require("dotenv").config;
+const dotenv = require("dotenv");
+dotenv.config();
+
 require("../src/utils/db");
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () =>
   console.log(`Started Express app on http://localhost:${PORT}/`)
 );
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,14 +1,19 @@
-require("dotenv").config;
+const dotenv = require("dotenv");
 const Sequelize = require("sequelize");
+
+dotenv.config();
+
 const sequelize = new Sequelize(
-  process.env.DBNAME,
-  process.env.USERNAME,
-  process.env.PASSWORD,
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
   {
     host: "localhost",
     dialect: "mysql",
     port: 3300,
   }
 );
+
+
 
 module.exports = { Sequelize, sequelize };
