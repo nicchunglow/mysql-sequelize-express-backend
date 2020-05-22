f3a58c658dfa8b21095b5e601f342d704dc85081
Nicholas Chung <iamnicholaschung@gmail.com>
2020-05-22 15:16:26 +0800

    Add commonStudent route

--- src/routes/teacherStudent.route.js
+++ src/routes/teacherStudent.route.js
@@ -32,6 +32,41 @@ const registerStudents = async (req, res, next) => {
   }
 };
 
+const commonStudents = async (req, res, next) => {
+  try {
+    const teacherQuery = req.query.teacher;
+    if (!teacherQuery) {
+      throw new Error("No teacher input");
+    }
+    const allTeacherStudents = await teacherModel.findOne({
+      where: {
+        teacher: teacherQuery,
+      },
+      attributes: ["teacher"],
+      include: {
+        model: studentModel,
+        attributes: ["students"],
+        through: { attributes: [] },
+      },
+    });
+    if (!allTeacherStudents) {
+      throw new Error("Invalid teacher input");
+    }
+    const onlyStudents = allTeacherStudents.students;
+    const studentsInArr = onlyStudents.map((eachStudent) => {
+      return eachStudent["students"];
+    });
+    const studentObj = { students: studentsInArr };
+    res.send(studentObj);
+  } catch (err) {
+    if (err.message === "Invalid teacher input" || "No teacher input") {
+      err.statusCode = 422;
+    }
+    next(err);
+  }
+};
+
 router.post("/register", registerStudents);
+router.get("/commonstudents", commonStudents);
 
 module.exports = router;
