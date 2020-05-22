15bb3b34a01732297c7895641344694e327e8a60
Nicholas Chung <iamnicholaschung@gmail.com>
2020-05-22 19:09:37 +0800

    Update getCommonStudents for single teacher and teachers

--- src/routes/teacherStudent.route.js
+++ src/routes/teacherStudent.route.js
@@ -1,6 +1,9 @@
 const teacherModel = require("../models/teacher.model");
 const studentModel = require("../models/student.model");
 const express = require("express");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
 const router = express.Router();
 
 const registerStudents = async (req, res, next) => {
@@ -33,33 +36,60 @@ const registerStudents = async (req, res, next) => {
 };
 
 const getCommonStudents = async (req, res, next) => {
+  const teacherQuery = req.query.teacher;
+  const numberofTeachers = teacherQuery.length;
   try {
-    const teacherQuery = req.query.teacher;
     if (!teacherQuery) {
       throw new Error("No teacher input");
     }
-    const allTeacherStudents = await teacherModel.findOne({
-      where: {
-        teacher: teacherQuery,
-      },
-      attributes: ["teacher"],
-      include: {
-        model: studentModel,
-        attributes: ["students"],
-        through: { attributes: [] },
-      },
-    });
-    if (!allTeacherStudents) {
-      throw new Error("Invalid teacher input");
+    const morethanOneTeacher = Array.isArray(teacherQuery);
+    if (morethanOneTeacher) {
+      const allTeacherStudents = await teacherModel.findAll({
+        where: {
+          teacher: teacherQuery,
+        },
+        attributes: ["teacher"],
+        include: {
+          model: studentModel,
+          attributes: ["students"],
+          through: { attributes: [] },
+        },
+        group: ["students"],
+        having: Sequelize.literal(`COUNT(students) = ${numberofTeachers}`),
+      });
+      const onlyStudents = allTeacherStudents[0].students;
+      const studentsInArr = onlyStudents.map((eachStudent) => {
+        return eachStudent["students"];
+      });
+      const studentJson = { students: studentsInArr };
+      res.send(allTeacherStudents);
+    } else {
+      const oneTeacherStudents = await teacherModel.findOne({
+        where: {
+          teacher: teacherQuery,
+        },
+        attributes: ["teacher"],
+        include: {
+          model: studentModel,
+          attributes: ["students"],
+          through: { attributes: [] },
+        },
+      });
+      if (!oneTeacherStudents) {
+        throw new Error("Teacher input unavailable or invalid.");
+      }
+      const onlyStudents = oneTeacherStudents.students;
+      const studentsInArr = onlyStudents.map((eachStudent) => {
+        return eachStudent["students"];
+      });
+      const studentJson = { students: studentsInArr };
+      res.send(studentJson);
     }
-    const onlyStudents = allTeacherStudents.students;
-    const studentsInArr = onlyStudents.map((eachStudent) => {
-      return eachStudent["students"];
-    });
-    const studentObj = { students: studentsInArr };
-    res.send(studentObj);
   } catch (err) {
-    if (err.message === "Invalid teacher input" || "No teacher input") {
+    if (
+      err.message === "Teacher input unavailable or invalid." ||
+      "No teacher input"
+    ) {
       err.statusCode = 422;
     }
     next(err);
